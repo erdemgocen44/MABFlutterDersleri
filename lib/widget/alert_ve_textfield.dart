4af130ddd21c226da5884e768ea9a781d4f1f11b
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-11-07 17:46:51 +0100

    alert tamam

--- lib/widget/alert_ve_textfield.dart
+++ lib/widget/alert_ve_textfield.dart
@@ -14,16 +14,46 @@ class _AlertVeTextFieldState extends State<AlertVeTextField> {
     super.dispose();
   }
 
+  void selamla() {
+    if (generalControl.currentState!.validate()) {
+      showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Selamlama Metniniz"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text("Merhaba ${yaziController.text}"),
+                  const Text("Sizi sistemimiz adına selamlıyorum"),
+                ],
+              ),
+            ),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Tamam"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AlertView ve TextField Kullanımı"),
+        title: const Text("AlertView ve TextField Kullanımı"),
       ),
       body: Form(
         key: generalControl,
         child: Padding(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -33,7 +63,20 @@ class _AlertVeTextFieldState extends State<AlertVeTextField> {
                     return "Lütfen bu kısmı boş bırakmayın";
                   }
                 },
-              )
+                controller: yaziController,
+                decoration:
+                    InputDecoration(hintText: "Lütfen isiminizi yazınız"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: ElevatedButton(
+                  onPressed: selamla,
+                  child: const Text(
+                    "Selamla",
+                    style: TextStyle(color: Colors.amber),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
